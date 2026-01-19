fa9ed0860075dfa6f2ecb4beb49a42dce94d3dc4
PhurinWork <phurinbas.work@gmail.com>
2026-01-19 16:43:17 +0700

    install document plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = () => ({
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: {
+        info: {
+          version: '3.0.0',
+          title: 'Documentation',
+          description: 'API documentation',
+        },
+        servers: [
+          {
+            url: 'http://localhost:1337',
+            description: 'Development server',
+          },
+        ],
+      },
+    },
+  },
+});
