0d126a6d4438979fe4bd1ec0c7659689d9c273ec
PhurinWork <phurinbas.work@gmail.com>
2026-02-05 10:44:17 +0700

    fix error 403

--- src/api/chat/routes/chat.js
+++ src/api/chat/routes/chat.js
@@ -4,20 +4,5 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::chat.chat', {
   config: {
-    find: {
-      policies: ['global::require-workspace-context'],
-    },
-    findOne: {
-      policies: ['global::require-workspace-context'],
-    },
-    create: {
-      policies: ['global::require-workspace-context'],
-    },
-    update: {
-      policies: ['global::require-workspace-context'],
-    },
-    delete: {
-      policies: ['global::require-workspace-context'],
-    },
   },
 });
--- src/api/knowledge-base/routes/knowledge-base.js
+++ src/api/knowledge-base/routes/knowledge-base.js
@@ -8,14 +8,5 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::knowledge-base.knowledge-base', {
   config: {
-    create: {
-      policies: ['global::require-workspace-context'],
-    },
-    update: {
-      policies: ['global::require-workspace-context'],
-    },
-    delete: {
-      policies: ['global::require-workspace-context'],
-    },
   },
 });
